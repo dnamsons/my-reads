b1ba2080404ea59644150f7fefd073e7295adc54
Davis Namsons <davisnamsons@gmail.com>
2020-05-02 20:01:21 +0300

    Fix: books without thumbnails

--- src/Book.js
+++ src/Book.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const BookShelfDropdown = ({ data, onChange }) => (
   <div className="book-shelf-changer">
-    <select value={data.shelf || "none"} onChange={(e) => onChange(data, e.target.value)}>
+    <select
+      value={data.shelf || "none"}
+      onChange={(e) => onChange(data, e.target.value)}
+    >
       <option value="move" disabled>
         Move to...
       </option>
@@ -14,28 +17,35 @@ const BookShelfDropdown = ({ data, onChange }) => (
   </div>
 );
 
-const BookTop = ({ data, onChange }) => (
-  <div className="book-top">
-    <div
-      className="book-cover"
-      style={{
-        width: 128,
-        height: 193,
-        backgroundImage: `url("${data.imageLinks.thumbnail}")`,
-      }}
-    />
-    <BookShelfDropdown data={data} onChange={onChange} />
-  </div>
-);
+const BookTop = ({ data, onChange }) => {
+  const backgroundProperty = data?.imageLinks
+    ? { backgroundImage: `url("${data.imageLinks.thumbnail}")` }
+    : {};
+
+  return (
+    <div className="book-top">
+      <div
+        className="book-cover"
+        style={{
+          width: 128,
+          height: 193,
+          ...backgroundProperty,
+        }}
+      />
+      <BookShelfDropdown data={data} onChange={onChange} />
+    </div>
+  );
+};
 
 const Book = ({ data, onChange }) => (
   <div className="book">
     <BookTop data={data} onChange={onChange} />
     <div className="book-title">{data.title}</div>
     <div className="book-authors">
-      {data.authors && data.authors.map((author, i) => (
-        <p key={`${data.id}-author-${i}`}>{author}</p>
-      ))}
+      {data.authors &&
+        data.authors.map((author, i) => (
+          <p key={`${data.id}-author-${i}`}>{author}</p>
+        ))}
     </div>
   </div>
 );
