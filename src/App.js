d87bea6204a00512d5fdf3bc78dc14f780540820
Davis Namsons <davisnamsons@gmail.com>
2020-05-03 07:05:29 +0300

    feat: Add a basic route for 404

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Search from './Search'
+import NotFound from './NotFound'
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route path="/search">
-            <Search />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
+          <Route path="/" exact component={Home} />
+          <Route path="/search" component={Search} />
+          <Route component={NotFound}/>
         </Switch>
       </div>
     </Router>
---
+++ src/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => (
+  <div className="list-books">
+    <div className="list-books-title">
+      <h1>MyReads</h1>
+    </div>
+
+    <h2 style={{ textAlign: "center" }}>404: This route was not found</h2>
+  </div>
+);
+
+export default NotFound;
